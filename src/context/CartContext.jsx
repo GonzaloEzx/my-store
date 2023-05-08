ba897634c9ba250dc133d46124620229276fba97
Gonza <gonzalo.estevez@farmacity.com.ar>
2023-05-07 22:40:38 -0300

    se modifico context

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -43,4 +43,49 @@ export const CartProvider = ({ children }) => {
     </CartContext.Provider>
   );
 };
-export default CartContext;
+
+// import { createContext, useState } from "react";
+
+// export const CartContext = createContext({
+//   cart: [],
+//   addItem: () => {},
+//   removeItem: () => {},
+//   clearCart: () => {},
+// });
+
+// export const CartProvider = ({ children }) => {
+//   const [cart, setCart] = useState([]);
+
+//   const addItem = (item, quantity) => {
+//     const itemIndex = cart.findIndex((prod) => prod.id === item.id);
+
+//     if (itemIndex === -1) {
+//       setCart([...cart, { ...item, quantity }]);
+//     } else {
+//       const updatedCart = [...cart];
+//       updatedCart[itemIndex].quantity += quantity;
+//       setCart(updatedCart);
+//     }
+//   };
+
+//   const removeItem = (itemId) => {
+//     setCart(cart.filter((prod) => prod.id !== itemId));
+//   };
+
+//   const clearCart = () => {
+//     setCart([]);
+//   };
+
+//   return (
+//     <CartContext.Provider
+//       value={{
+//         cart,
+//         addItem,
+//         removeItem,
+//         clearCart,
+//       }}
+//     >
+//       {children}
+//     </CartContext.Provider>
+//   );
+// };
